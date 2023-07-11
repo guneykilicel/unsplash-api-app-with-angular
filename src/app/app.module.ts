661790aa1d851a2b0868d30bccc1bdf9ad918495
guneykilicel <kilicelguney@gmail.com>
2023-07-11 15:49:34 +0300

    iskelet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DisplayphotoComponent } from './displayphoto/displayphoto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayphotoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/displayphoto/displayphoto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayphoto',
+  templateUrl: './displayphoto.component.html',
+  styleUrls: ['./displayphoto.component.css']
+})
+export class DisplayphotoComponent {
+
+}
